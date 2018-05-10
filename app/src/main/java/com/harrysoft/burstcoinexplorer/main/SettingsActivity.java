3e71a54b367f37b9150b311781a65322bad90d99
harry1453 <harryjamesphillips@gmail.com>
2018-05-10 22:45:28 +0100

    Fix Settings Back Button

--- app/src/main/java/com/harrysoft/burstcoinexplorer/main/SettingsActivity.java
+++ app/src/main/java/com/harrysoft/burstcoinexplorer/main/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.harrysoft.burstcoinexplorer.main;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.harrysoft.burstcoinexplorer.R;
 
@@ -17,6 +18,19 @@ public class SettingsActivity extends AppCompatActivity {
         }
 
         getSupportFragmentManager().beginTransaction().add(R.id.settings_container, new SettingsFragment()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
