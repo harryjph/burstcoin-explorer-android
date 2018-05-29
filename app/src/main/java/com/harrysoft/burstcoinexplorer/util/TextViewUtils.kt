7f123eb3b024bacfb07ac0a626313db42c496926
Harry <37041552+harry1453@users.noreply.github.com>
2018-05-29 11:45:11 +0100

    Improve TextViewUtils (#45)
    * Move Hyperlinking to global function
    
    * Fix weird bug caused by having two different data sources :/

--- app/src/main/java/com/harrysoft/burstcoinexplorer/util/TextViewUtils.kt
+++ app/src/main/java/com/harrysoft/burstcoinexplorer/util/TextViewUtils.kt
@@ -3,14 +3,16 @@ package com.harrysoft.burstcoinexplorer.util
 import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.URLSpan
+import android.view.View
 import android.widget.TextView
 
 object TextViewUtils {
     @JvmStatic
-    fun makeTextViewHyperlink(tv: TextView) {
+    fun setupTextViewAsHyperlink(textView: TextView, onClickListener: View.OnClickListener) {
         val ssb = SpannableStringBuilder()
-        ssb.append(tv.text)
+        ssb.append(textView.text)
         ssb.setSpan(URLSpan("#"), 0, ssb.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        tv.setText(ssb, TextView.BufferType.SPANNABLE)
+        textView.setText(ssb, TextView.BufferType.SPANNABLE)
+        textView.setOnClickListener(onClickListener);
     }
 }
